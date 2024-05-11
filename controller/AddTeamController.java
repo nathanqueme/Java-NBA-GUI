795b5187693c6590a9770f5ef99fb91972c3c00a
nqueme <nathan@qsstudio.co>
2024-05-12 02:42:43 +1000

    nav: error from add team

--- controller/AddTeamController.java
+++ controller/AddTeamController.java
@@ -2,8 +2,15 @@ package controller;
 
 import javafx.fxml.*;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import model.Teams;
 import model.Team;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import au.edu.uts.ap.javafx.*;
 
 
@@ -15,7 +22,24 @@ public class AddTeamController extends Controller<Teams> {
 	private String getName() { return nameTf.getText(); }
 	private void setName(String name) { nameTf.setText(name); }
 
+    // UTILITY METHOD   --------------------------------------
+    public static Stage newStage(String imageName) {
+        Stage stage = new Stage();
+        stage.setX(ViewLoader.X + 601);
+        stage.setY(ViewLoader.Y);
+        stage.getIcons().add(new Image("/view/" + imageName));
+        return stage;
+    }
+    // -------------------------------------------------------
+
 	@FXML private void addTeam() {
+		try {
+            Stage stage = newStage("error.png");
+            ViewLoader.showStage(getTeams(), "/view/error.fxml", "Error!", stage);
+        } catch (IOException ex) {
+            Logger.getLogger(SeasonController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         Team team = new Team(getName());
 		getTeams().addTeam(team);
 		stage.close();
--- controller/ErrorController.java
+++ controller/ErrorController.java
@@ -1,7 +1,16 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import au.edu.uts.ap.javafx.Controller;
 
-
-public class ErrorController  {
+public class ErrorController extends Controller<Object> {
     
+    @FXML private Button closeButton;
+
+    @FXML
+    public void close() {
+        stage.close();
+    }
 }
+
