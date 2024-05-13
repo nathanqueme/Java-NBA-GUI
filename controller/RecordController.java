a326ba3e5525c4a9c851101af2d209fc7c38785a
nqueme <nathan@qsstudio.co>
2024-05-14 01:51:09 +1000

    nav(RecordView): fxml and controller
    Also fix the width of list in the season round view

--- controller/RecordController.java
+++ controller/RecordController.java
@@ -1,15 +1,30 @@
 package controller;
 
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import model.Season;
+import model.Record;
+import au.edu.uts.ap.javafx.*;
 
-public class RecordController {
+public class RecordController extends Controller<Season> {
+
+    @FXML private TableView<Record> groupsTv;
+    @FXML private TableColumn<Record, String> roundColumn;
+    @FXML private TableColumn<Record, String> gameColumn;
+    @FXML private TableColumn<Record, String> winColumn;
+    @FXML private TableColumn<Record, String> loserColumn;
+    @FXML private Button closeButton;
+
+    public final Season getSeason() {
+        return model;
+    }
+
+    @FXML public void initialize() {
+        groupsTv.setItems(getSeason().record());
+    }
 
 
-    
+    @FXML public void close() {
+        stage.close();
+    }
 }
-
-
-
-
-
-
-
